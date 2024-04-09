cf5df6e2d120cec4749888b5ff4f3618686d433b
Sagar Sanjay Lekkalwar <141488344+SagarLekkalwar0409@users.noreply.github.com>
2024-04-09 19:43:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+	var weatherIcon = document.getElementById("weather-icon");
+	
+	var val = document.getElementById("wc").value;
+	  switch (val) {
+      case 'Clouds':
+          weatherIcon.src = "images/clear.png";
+          break;
+      case 'Clear':
+          weatherIcon.src = "images/sun.png";
+          break;
+      case 'Rain':
+          weatherIcon.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgDW_NdwvxV796rkFf43qmUDiTQePn5dg7PDfn1SijfpjtB0AWJMBcifU6LWyW7iOtjZhfqIJnKEGQ1PwbbXS7NoKMSAmvy7i2ljWXMYLue3EBIBBR2qTFbs6QCe5eoFr2CU9WzCVJ8u0J3z3eAo3Ajv1LXamZASFtbj9sA_gD-Kp3hfgAk17Xh17RoLQ/s320/rainy.png";
+          break;
+      case 'Mist':
+          weatherIcon.src = "images/clear.png";
+          break;
+      case 'Snow':
+          weatherIcon.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj-P3iT_uQK95qFY4h7QGdEtbRc1aVQo9BZy0ZWyPBvCNrP-4wnRStw0xYj9e4xa4ZlYISeNZqVJ33UP4YukR4jBennDD_obIN4QxYNZHdzG_z6_MNL2U08wMXwdFhtfvitW5LGiHgrwMJFC8QJFqbSO3woGSBqOdagGxaEQ20_S31Gc-GYL4vYzPzaPw/s320/snow.png";
+          break;
+      case 'Haze':
+          weatherIcon.src = "images/clear.png";
+          break;
+  }
